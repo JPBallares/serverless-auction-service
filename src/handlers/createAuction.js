1282d443f63db172b9e41bcbe1e47ea70ea32094
Justine Ballares <ballaresjustine@gmail.com>
2023-04-02 10:36:43 +0800

    added uuid and saving to dynamo db

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,16 +1,28 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
+const db = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
   const { title } = JSON.parse(event.body); // get the title from the data
 
   // create an auction object
   const auction = {
+    id: uuid(),
     title,
     status: 'OPEN',
     createdAt: Date.now()
   };
 
+  // store it in the database
+  await db.put({
+    TableName: 'AuctionsTable',
+    Item: auction,
+  }).promise(); // use promises (async/await) instead of default callbacks
+
   return {
     statusCode: 201,
-    body: JSON.stringify({ auction }),
+    body: JSON.stringify(auction),
   };
 }
 
